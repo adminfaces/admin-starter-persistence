95fb54c1479439869814fd2873c81e4bd5793270
rmpestano <rmpestano@gmail.com>
2017-08-25 17:57:17 -0300

    - updates persistence to RC4 - adds searchCriteria in list page

--- src/main/java/com/github/adminfaces/starter/bean/CarListMB.java
+++ src/main/java/com/github/adminfaces/starter/bean/CarListMB.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import static com.github.adminfaces.persistence.util.Messages.addDetailMessage;
+import static com.github.adminfaces.template.util.Assert.has;
 
 
 /**
@@ -32,7 +33,7 @@ public class CarListMB extends CrudMB<Car> implements Serializable {
 
     @Inject
     public void initService() {
-       setCrudService(carService);
+        setCrudService(carService);
     }
 
 
@@ -58,4 +59,66 @@ public class CarListMB extends CrudMB<Car> implements Serializable {
         addDetailMessage(numCars + " cars deleted successfully!");
     }
 
+    public String getSearchCriteria() {
+        StringBuilder sb = new StringBuilder(21);
+
+        String nameParam = null;
+        Car carFilter = filter.getEntity();
+        if (filter.hasParam("name")) {
+            nameParam = filter.getStringParam("name");
+        } else if (has(carFilter) && carFilter.getName() != null) {
+            nameParam = carFilter.getName();
+        }
+
+        if(has(nameParam)) {
+              sb.append("<b>name</b>: " + nameParam + ", ");
+        }
+
+        String modelParam = null;
+        if (filter.hasParam("model")) {
+             modelParam = filter.getStringParam("model");
+        } else if (has(carFilter) && carFilter.getModel() != null) {
+             modelParam = carFilter.getModel();
+        }
+
+        if(has(modelParam)) {
+              sb.append("<b>model</b>: " + modelParam + ", ");
+        }
+
+        Double priceParam = null;
+        if (filter.hasParam("price")) {
+           priceParam = filter.getDoubleParam("price");
+        } else if (has(carFilter) && carFilter.getModel() != null) {
+           priceParam = carFilter.getPrice();
+        }
+
+         if(has(priceParam)) {
+              sb.append("<b>price</b>: " + priceParam + ", ");
+        }
+
+        if (filter.hasParam("minPrice")) {
+            sb.append("<b>"+messages.getMessage("label.minPrice")+"</b>: " + filter.getParam("minPrice") + ", ");
+        }
+
+        if (filter.hasParam("maxPrice")) {
+            sb.append("<b>"+messages.getMessage("label.maxPrice")+"</b>: " + filter.getParam("maxPrice") + ", ");
+        }
+
+        int commaIndex = sb.lastIndexOf(",");
+
+        if (commaIndex != -1) {
+            sb.deleteCharAt(commaIndex);
+        }
+
+        if (sb.toString().trim().isEmpty()) {
+            return "No search criteria";
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean keepFiltersInSession() {
+        return false;
+    }
 }
