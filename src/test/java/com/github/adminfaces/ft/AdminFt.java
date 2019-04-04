8bea86e22e92f00fe83fdde4817c82ba63652ebe
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-04-04 12:18:29 -0300

    fixes tests

--- src/test/java/com/github/adminfaces/ft/AdminFt.java
+++ src/test/java/com/github/adminfaces/ft/AdminFt.java
@@ -47,6 +47,7 @@ public class AdminFt {
         WebArchive war = Deployments.createDeployment();
         MavenResolverSystem resolver = Maven.resolver();
         war.addAsLibraries(resolver.loadPomFromFile("pom.xml").resolve("com.github.adminfaces:admin-template").withTransitivity().asFile());
+        war.addAsLibraries(resolver.loadPomFromFile("pom.xml").resolve("org.omnifaces:omnifaces").withoutTransitivity().asSingleFile());
         war.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class).importDirectory("src/main/webapp").as(GenericArchive.class), "/", Filters.include(".*\\.(xml|xhtml|html|css|js|png|jpg|gif)$"));
 
         System.out.println(war.toString(true));
